2910018a002eccaad943e7a67500c29e2013bc21
lidianyuan <beam.analyman@gmail.com>
2019-08-22 10:45:19 +0800

    pyrevitshellScript

---
+++ CreateInstance.cs
@@ -0,0 +1,11 @@
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using System.Collections.Generic;
+using System.Collections;
+
+namespace NaanRevit {
+    static public class CreateInstance
+    {
+    }
+}
