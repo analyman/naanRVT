25f59b32bbce47fb5ef97637ba7013269ca28f4d
lidianyuan <beam.analyman@gmail.com>
2019-09-02 21:20:15 +0800

    hellolegeworld

--- dynamo/csharp/src/Geometry/Geometry.cs
+++ dynamo/csharp/src/Geometry/Geometry.cs
@@ -9,7 +9,7 @@ using System.Numerics;
 
 namespace NaaN.dynamo.Geometry
 {
-    public class AFTransform
+    public class AFTransform: IGeometry
     {
         private double[,] transM;
         private double[]  transV;
@@ -123,18 +123,13 @@ namespace NaaN.dynamo.Geometry
             }
             Matrix<double> revM = this.mathnetMatrix.Inverse();
             Vector<double> revV =  - revM * this.mathnetVector;
-            double[,] xarray = new double[3, 3];
-            for(int i = 0; i<3; i++){
-                for(int j = 0; j<3; j++){
-                    xarray[j, i] = revM.Storage[j, i];
-                }
-            }
-            revM.Storage.AsArray();
+            double[,] xarray = geometryUtils.MathMatrixToArray(revM);
             return new AFTransform(
                 xarray,
                 revV.Storage.AsArray()
             );
         }
+
 
         public override string ToString()
         {
@@ -162,10 +157,95 @@ namespace NaaN.dynamo.Geometry
             return new AFTransform(new double[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } },
                                    new double[3] { pt.X, pt.Y, pt.Z });
         }
+
+        public static AFTransform ByRotateAroundVector(CSVector vec, double theta)
+        {
+            double[,] rotateTransformArray = geometryUtils.RotateThetaToMatrix(theta);
+
+            Vector<double> mathnetV = Vector<double>.Build.DenseOfArray(new double[3]{vec.X, vec.Y, vec.Z});
+            double[,] A = geometryUtils.VectorToStardardZTransform(mathnetV);
+            double[,] AI = geometryUtils.inverseArrayMatrix(A);
+            return (new AFTransform(A)) * (new AFTransform(rotateTransformArray)) * (new AFTransform(AI));
+        }
+
+        public static AFTransform operator*(AFTransform af1, AFTransform af2)
+        {
+            Matrix<double> af3Matrix = af1.mathnetMatrix * af2.mathnetMatrix;
+            Vector<double> af3Vector = af2.mathnetMatrix * af1.mathnetVector + af2.mathnetVector;
+            return new AFTransform(geometryUtils.MathMatrixToArray(af3Matrix), af3Vector.Storage.AsArray());
+        }
+
+        public  void AffineTransform(AFTransform aft)
+        {
+             AFTransform result =  this * aft;
+             this.transM = result.transM;
+             this.transV = result.transV;
+        }
     }
 
     public interface IGeometry 
     {
-        void Transform(AFTransform aft);
+        void AffineTransform(AFTransform aft);
+    }
+
+    internal static class geometryUtils
+    {
+        public static double[,] RotateThetaToMatrix(double theta)
+        {
+            double[,] ret = new double[3,3]{
+                {Math.Cos(theta), Math.Sin(theta), 0},
+                {-Math.Sin(theta), Math.Cos(theta), 0},
+                {0, 0, 1}
+            };
+            return ret;
+        }
+
+        public static double[,] VectorToStardardZTransform(Vector<double> vec)
+        {
+            vec = vec / vec.L2Norm();
+            double[] vec_array = vec.Storage.AsArray();
+            double[] zero_array = new double[3]{0, 0, 0};
+
+            Matrix<double> problem = Matrix<double>.Build.DenseOfRowArrays(new double[][] {vec_array, zero_array, zero_array});
+            Vector<double>[] kernels = problem.Kernel();
+
+            if(kernels.Length != 2)
+            {
+                throw new Exception("It's impossible, except parameter vec is zero");
+            }
+
+            double[] v1 = kernels[0].Storage.AsArray();
+            double[] v2 = kernels[1].Storage.AsArray();
+
+            return new double[3,3]
+            {
+                {vec_array[0], vec_array[1], vec_array[2]},
+                {v1[0], v1[1], v1[2]},
+                {v2[0], v2[1], v2[2]}
+            };
+        }
+
+        public static double[,] inverseArrayMatrix(double[,] darray)
+        {
+            if(darray.Length == 0 || darray.GetLength(0) != darray.GetLength(1))
+            {
+                throw new ArgumentException("Expect a square two dimension array");
+            }
+            Matrix<double> x = Matrix<double>.Build.DenseOfArray(darray);
+            Matrix<double> x_inv = x.Inverse();
+            return MathMatrixToArray(x_inv);
+        }
+
+        public static double[,] MathMatrixToArray(Matrix<double> x)
+        {
+            double[,] xarray = new double[x.RowCount, x.ColumnCount];
+            for(int i = 0; i<x.ColumnCount; i++){
+                for(int j = 0; j<x.RowCount; j++){
+                    xarray[j, i] = x.Storage[j, i];
+                }
+            }
+            return xarray;
+        }
+
     }
 }
